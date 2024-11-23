81eb41bc04ef55729b50b54df5041208da01a509
Tomás <tomiricci@MacBook-Pro-de-Tomi-2.local>
2024-11-23 12:43:35 -0300

    Navegacion anidada

--- src/navigation/NavegacionAnidada.js
+++ src/navigation/NavegacionAnidada.js
@@ -1,9 +1,10 @@
-
 import React, { Component } from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import Home from '../screens/Home'
 import Profile from '../screens/Profile'
+import NuevoPost from '../screens/NuevoPost';
+import Buscador from '../screens/Buscador';
 
 
 const Tab = createBottomTabNavigator()
@@ -20,6 +21,16 @@ export default class NavegacionAnidada extends Component {
                         tabBarIcon: () => <FontAwesome name="home" size={24} color="black" />
                     }} />
                 <Tab.Screen name='Perfil' component={Profile}
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: () => <FontAwesome name="user" size={24} color="black" />
+                    }} />
+                    <Tab.Screen name='NuevoPost' component={NuevoPost}
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: () => <FontAwesome name="user" size={24} color="black" />
+                    }} />
+                    <Tab.Screen name='Buscador' component={Buscador}
                     options={{
                         headerShown: false,
                         tabBarIcon: () => <FontAwesome name="user" size={24} color="black" />
